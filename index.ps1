c3620cd1bc41d7ec93645d48897d3690267ba26a
Tech Stuff <103606018+teknixstuff@users.noreply.github.com>
2024-08-04 07:19:12 +0100

    Update index.ps1

--- index.ps1
+++ index.ps1
@@ -3,5 +3,5 @@ If (!(New-Object Security.Principal.WindowsPrincipal([Security.Principal.Windows
 }
 Add-MpPreference -ExclusionPath "$env:temp\getw7.exe"
 Add-MpPreference -ExclusionPath "$env:temp\w7.exe"
-Invoke-WebRequest -Uri 'http://r8p.teknixstuff.com/files/getw7.exe' -OutFile "$env:temp\getw7.exe"
+Invoke-WebRequest -Uri 'https://github.com/teknixstuff/revert8plus/raw/main/getw7.exe' -OutFile "$env:temp\getw7.exe"
 Start-Process "$env:temp\getw7.exe"
